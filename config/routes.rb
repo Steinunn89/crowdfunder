613a5592babadff8a8843e09143232174ef4841b
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-03 15:33:09 -0500

    nested the routes pledges and rewards to the project model

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-
-
-  resources :projects
   root 'projects#index'
+
+  resources :projects do
+    resources :pledges
+    resources :rewards, only: [:new, :create, :show, :destroy]
+  end
+
   resources :users
   resources :user_sessions, only: [:new, :create, :destroy]
 
   get 'login' => 'user_sessions#new', as: 'login'
   post 'logout' => 'user_sessions#destroy', as: 'logout'
-  resources :rewards, only: [:new, :create, :show, :destroy]
-  resources :pledges
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
