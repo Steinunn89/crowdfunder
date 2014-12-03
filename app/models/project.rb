e7891b4e0680e3d33e165e89051cc7b59335adb0
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-03 17:32:00 -0500

    added goal to the project params, defined a method in the project model that calculates the amount of pledges, and displayed the amount in the project show.

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,10 @@ class Project < ActiveRecord::Base
 	
 	accepts_nested_attributes_for :rewards, reject_if: :all_blank, allow_destroy: true
 	
+	def amount_raised
+		self.pledges.sum(:amount)
+	end
+
 	def match_reward(amount)
 		rewards.where(['amount <= ?', amount]).order('amount desc').first
 	end
