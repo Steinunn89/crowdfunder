46ae7551b8f5ff4610d81b40668645662429f892
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-04 17:53:13 -0500

    indented properly

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,36 +1,38 @@
 class ProjectsController < ApplicationController
-
-before_action :require_login, except: :index 
-load_and_authorize_resource
-def index
-	@projects = Project.all
-end
-
-def new
-	@project = Project.new
-end
-
-def create
-	@project = Project.new(project_params)
+	before_action :require_login, except: :index 
+	load_and_authorize_resource
 	
-	if @project.save
-
-		redirect_to projects_url
-		
-	else
-		render :new
+	def index
+		@projects = Project.all
 	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(project_params)
 		
-end
+		if @project.save
 
-def show
-	@project = Project.find(params[:id])
-end
+			redirect_to projects_url
+			
+		else
+			render :new
+		end
+			
+	end
 
-private
+	def show
+		@project = Project.find(params[:id])
+		@pledge = Pledge.new(amount: params[:amount])
+		@reward = @project.match_reward(@pledge.amount)
+	end
 
-def project_params
-	params.require(:project).permit(:name, :goal, :description, :end_date, rewards_attributes: [:id, :amount, :description, :_destroy])
-	
-end
+	private
+
+	def project_params
+		params.require(:project).permit(:name, :goal, :description, :end_date, rewards_attributes: [:id, :amount, :description, :_destroy])
+		
+	end
 end
