6a641464391c5defc2363e4efd13e2fef2824f63
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-04 16:44:48 -0500

    Created a User show view that displays the user name, projects pledged and amount pledged for each

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def show
+		@user = User.find(params[:id])
+	end
 	private
 	def user_params
 		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
